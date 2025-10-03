e04d3d12aa62efd9d27a97b661b07c4ffb8ab7de
victorroddev <javier.rod.dev@gmail.com>
2025-10-02 19:45:24 -0600

    fix

--- frontend/src/components/Menu/index.jsx
+++ frontend/src/components/Menu/index.jsx
@@ -9,18 +9,18 @@ const Menu = ({ isOpen }) => {
     return (
         <div className={`menu ${isOpen ? 'menu-visible' : 'menu-hidden'}`}>
             <ul className='links'>
-                <li><a href="">Preguntas frecuentes</a></li>
+                <li><a href="#Faq">Preguntas frecuentes</a></li>
                 <div className='divider-link'></div>
-                <li><a href="">Contacto</a></li>
+                <li><a href="Contact">Contacto</a></li>
                 <div className='divider-link' ></div>
                 <li className='icons-container'>
-                    <a href="">
+                    <a href="www.facebook.com">
                         <FaFacebookF/>
                     </a>
-                    <a href="">
+                    <a href="www.instagram.com">
                         <FaInstagram />
                     </a>
-                    <a href="">
+                    <a href="www.tiktok.com">
                         <FaTiktok />
                     </a>
                 </li>
